28ccca264817d5c54dba86f10744128e98c7e111
sarakara <gulcinsarankara@hotmail.com>
2022-12-06 14:14:46 -0500

    Ticketing rest api

--- src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -1,7 +1,10 @@
 package com.cydeo.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
