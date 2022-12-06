e574a529c0528d1eee73f29e4b985a7feda3769f
sarakara <gulcinsarankara@hotmail.com>
2022-12-06 14:39:30 -0500

    Ticketing rest api

--- src/main/java/com/cydeo/config/SwaggerConfig.java
+++ src/main/java/com/cydeo/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
