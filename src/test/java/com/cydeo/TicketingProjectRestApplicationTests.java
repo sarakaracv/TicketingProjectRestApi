6a154e5f0e4ec99811cad68881065544dd62ec13
sarankara <gulcinsarankara@hotmail.com>
2023-07-09 20:25:15 -0400

    Spring Rest Keycloak OAuth2

--- src/test/java/com/cydeo/TicketingProjectRestApplicationTests.java
+++ src/test/java/com/cydeo/TicketingProjectRestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cydeo.ticketingprojectrestapi;
+package com.cydeo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TicketingProjectRestApiApplicationTests {
+class TicketingProjectRestApplicationTests {
 
     @Test
     void contextLoads() {
